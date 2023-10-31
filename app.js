eb9f5811ff4ae8f46ccc2b1e8fd6fb8e32168e92
GalisGalisteo <galis.travelling@gmail.com>
2023-10-31 18:08:25 +0100

    finishing ex with bonus

--- app.js
+++ app.js
@@ -23,6 +23,19 @@ const updateData = async () => {
     document.querySelector('#score-2').textContent = data[1].puntos;
     document.querySelector('#third-position').textContent = data[2].nombre;
     document.querySelector('#score-3').textContent = data[2].puntos;
+    const players = document.querySelector('#players');
+    players.innerHTML = '';
+    for (let i = 3; i < data.length; i++) {
+        const player = document.createElement('div');
+        player.classList.add('player');
+        player.innerHTML =
+            `
+            <div class="place-number">${i + 1}th</div>
+            <div id="${i + 1}-position" class="player-name">${data[i].nombre}</div>
+            <div class="score"><span id="score-${i + 1}">${data[i].puntos}</span> palabras adivinadas</div>
+        `;
+        players.appendChild(player);
+    }
 }
 
 document.querySelector('#update-btn').addEventListener('click', () => {
